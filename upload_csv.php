182939f49855e22040220aea78c54a7302f145ec
Baemull <baemull@gmx.de>
2015-07-09 22:33:23 +0200

    added upload button v0.1

---
+++ upload_csv.php
@@ -0,0 +1,24 @@
+<?php
+if ( isset($_POST["submit"]) ) {
+  
+
+   if ( isset($_FILES["file"])) {
+
+            //if there was an error uploading the file
+        if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+
+        }
+        else {
+                 //Print file details
+             echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+             echo "Type: " . $_FILES["file"]["type"] . "<br />";
+             echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+             echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+        }
+     } else {
+             echo "No file selected <br />";
+     }
+}
+ ?>
