1c6c36f807bb24735dc6378e83ca6ea73715223f
Baemull <baemull@gmx.de>
2015-07-09 21:12:09 +0200

    added new index site and materializecss

---
+++ get_udp_gps.php
@@ -0,0 +1,10 @@
+﻿<?php
+$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+ 
+if (!$socket) die('Unable to create socket');
+if (!socket_bind($socket, "192.168.43.215", 8087)) die('Unable to bind socket');
+ 
+    $data = @socket_read($socket, 15);
+ 
+    echo $data . "\n";  // oder in eine Datei schreiben
+?>
